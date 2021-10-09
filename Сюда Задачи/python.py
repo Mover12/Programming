5ecd69cadfcd8f79172c5a012618c87ed97e4a78
Mover12 <76180380+Mover12@users.noreply.github.com>
2021-10-09 18:45:21 +0300

    Update python.py

--- Сюда Задачи/python.py
+++ Сюда Задачи/python.py
@@ -5,3 +5,12 @@ num = 0
 for b in str(a):
     num += int(b)
 print(num)
+#это самый простой путь можно даже сказать
+#решение по тупому(не в обиду) и привыкай все задачи 
+#или программы писать в фунции, мой вариант выглядит так разбери как работает:
+
+def sum_num(num):
+    num_list = [int(num) for num in str(num)]
+    return print(sum(num_list))
+
+sum_num(a)
